8c07835e8d011b0d10e8a627eb2113b35c20ede9
Lucas Jundi Hikazudani <lhjundi@proton.me>
2024-10-04 18:59:49 -0300

    add authors name

--- src/main/java/com/lhjundi/car_workshop/CarWorkshopApplication.java
+++ src/main/java/com/lhjundi/car_workshop/CarWorkshopApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarWorkshopApplication {
 
+	//Lucas Jundi Hikazudnai
+	//Vitor Mendonça Callichio
 	public static void main(String[] args) {
 		SpringApplication.run(CarWorkshopApplication.class, args);
 	}
